8a12cf744eaaeae05bb513cb30c3819029fc733a
AdamWalker-Tonkean <adam.walker@tonkean.com>
2022-09-22 08:39:18 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//prettier-ignore
+const colors = ['#ff0000','#ffff00','#ff8800',
+                '#00ff00','#ff00ff','#00ffff',
+                '#0000ff','#000000','#ffffff',
+                '#ce00ff','#14930a','#0af89d'];
+
+document.querySelector("button").addEventListener("click", function () {
+  window.location.href = "https://www.google.com";
+});
